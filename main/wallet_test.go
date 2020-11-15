647f6b755af5a82b6aaf693442d7874910981ebd
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-11-15 08:54:14 +0100

    wallet pointers first green test

---
+++ main/wallet_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	
+	want := 10
+
+	if got != want {
+			t.Errorf("got %d want %d", got, want)
+	}
+}
