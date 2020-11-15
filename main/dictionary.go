b965b5268e3b843648e469779e6b3dc1f8127a7d
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-11-15 13:05:01 +0100

    add Update method to Dictionary

--- main/dictionary.go
+++ main/dictionary.go
@@ -5,6 +5,7 @@ type Dictionary map[string]string
 const (
 	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -34,4 +35,19 @@ func (d Dictionary) Search(word string) (string, error) {
     }
 
     return definition, nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+			return ErrWordDoesNotExist
+	case nil:
+			d[word] = definition
+			return nil
+	default:
+			return err
+	}
+
 }
